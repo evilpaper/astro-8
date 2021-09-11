a026a98bc5167e0955c0236cac366ebe3206310a
evilpaper <pellburt@gmail.com>
2021-09-11 10:33:54 +0200

    Load spritesheet and draw it on canvas

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,16 @@
+function loadImage(url) {
+  return new Promise((resolve, reject) => {
+    const image = new Image();
+    image.src = url;
+    image.addEventListener("load", () => resolve(image));
+  });
+}
+
 const canvas = document.getElementById("screen");
 const context = canvas.getContext("2d");
 
 context.fillRect(0, 0, 50, 50);
+
+loadImage("./img/spritesheet.png").then((image) => {
+  context.drawImage(image, 0, 0);
+});
