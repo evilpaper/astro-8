9e49bf7d3524b5b174d95a5a381042d70d3abf61
evilpaper <pellburt@gmail.com>
2021-09-12 12:07:39 +0200

    Defined space sprite and paint background

--- public/js/main.js
+++ public/js/main.js
@@ -7,5 +7,12 @@ const context = canvas.getContext("2d");
 loadImage("./img/spritesheet.png").then((image) => {
   const sprites = new SpriteSheet(image, 16, 16);
   sprites.define("ground", 0, 35);
+  sprites.define("space", 66, 35);
+
+  for (let x = 0; x < 24; ++x) {
+    for (let y = 0; y < 16; ++y) {
+      sprites.draw("space", context, x * 16, y * 16);
+    }
+  }
   sprites.draw("ground", context, 100, 100);
 });
