fdc033407e742006dc470de11bab123292e44b34
evilpaper <pellburt@gmail.com>
2021-10-15 07:20:59 +0200

    Add clarification comment to spritesheet

--- public/js/SpriteSheet.js
+++ public/js/SpriteSheet.js
@@ -1,3 +1,13 @@
+/*
+Defines the structure of a Spritesheet. Used to create instances of spritesheets.
+The image is the image we use for the spritesheet. The image can have any size.
+The width and height is the width and height of the individual sprites insisde 
+the spritesheet. They are needed in order to define and draw the sprites. 
+Example, if the width and height is 16 the spritesheet would contain indefinite number of sprites
+with the width and height of 16. Not confuse width and heigth with width and height of image used to 
+define the sprites. That is a compeltly other thing.
+*/
+
 export default class SpriteSheet {
   constructor(image, width, height) {
     this.image = image;
